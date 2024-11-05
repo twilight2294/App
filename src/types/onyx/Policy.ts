d62b346b82c771babc3388c98c7eae2fc5725ad4
twilight2294 <twitteruser902@gmail.com>
2024-11-05 12:24:25 +0000

    Test commit to verify GPG signing

--- src/types/onyx/Policy.ts
+++ src/types/onyx/Policy.ts
@@ -1419,9 +1419,11 @@ type PolicyReportField = {
     /** Default value assigned to the field */
     defaultValue: string;
 
+    
+
     /** Unique id of the field */
     fieldID: string;
-    
+
 
     /** Position at which the field should show up relative to the other fields */
     orderWeight: number;
